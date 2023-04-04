82c46de34352a238824e0e3f46f5aee73192680a
s_shokoofa <sd3power@gmail.com>
2023-04-04 21:49:36 +0330

    some modules debuged

--- ALU.v
+++ ALU.v
@@ -5,22 +5,24 @@ input carry;
 output [31:0]out;
 output reg[3:0] status_out;
 reg [32:0]outt;
-always(*)begin
-    v=0;
-    case(cmd):
+
+always@(*) begin
+    status_out[3]=0;
+    case(cmd)
     4'b0001:outt=src2;
     4'b1001:outt=~{src2};
-    4'b0010:begin outt=src1+src2; v=(src1[31] && src2[31] && ~outt[31])||(~src1[31] && ~src2[31] && outt[31]); end
-    4'b0011:begin outt=src1+src2+carry; v=(src1[31] && src2[31] && ~outt[31])||(~src1[31] && ~src2[31] && outt[31]); end
-    4'b0100:begin outt=src1-src2; v=(src1[31] && ~src2[31] && ~outt[31])||(~src1[31] && src2[31] && outt[31]); end
-    4'b0101:begin outt=src1-src2-~carry;v=(src1[31] && ~src2[31] && ~outt[31])||(~src1[31] && src2[31] && outt[31]); end
+    4'b0010:begin outt=src1+src2;  status_out[3]=(src1[31] && src2[31] && ~outt[31])||(~src1[31] && ~src2[31] && outt[31]); end
+    4'b0011:begin outt=src1+src2+carry;  status_out[3]=(src1[31] && src2[31] && ~outt[31])||(~src1[31] && ~src2[31] && outt[31]); end
+    4'b0100:begin outt=src1-src2;  status_out[3]=(src1[31] && ~src2[31] && ~outt[31])||(~src1[31] && src2[31] && outt[31]); end
+    4'b0101:begin outt=src1-src2-~carry; status_out[3]=(src1[31] && ~src2[31] && ~outt[31])||(~src1[31] && src2[31] && outt[31]); end
     4'b0110:outt=src1&src2;
     4'b0111:outt=src1|src2;
     4'b1000:outt=src1^src2;
     endcase
-    status_out[0]=&{~outt}
+    status_out[0]=&{~outt};
     status_out[1]=outt[32];
     status_out[2]=outt[31];
+
 end
 // z c n v
 assign out=outt[31:0];
--- adder.v
+++ adder.v
@@ -1,3 +1,3 @@
-module adder(input [31:0]a,b,output[31:0]out)
+module adder(input [31:0]a,b,output[31:0]out);
 assign out=a+b;
 endmodule
