f3633507a20fa1def0e4389c3b45ff1ea47b4c79
s_shokoofa <sd3power@gmail.com>
2023-04-06 14:37:00 +0330

    ARM prototype was written

--- ARM.v
+++ ARM.v
@@ -1,5 +1,7 @@
-module datapath(clk,rst);
+module ARM(clk,rst);
+
 input clk,rst;
+
 IF IF_stg(.clk(clk),.rst(rst),.freeze(),.Branch_taken(),.Branch_Addr(),.PC(),.Instruction());
 IF_reg IF_register(.clk(clk),.rst(rst),.freeze(),.flush(),.PC_in(),.Instruction_in(),.PC(),.Instruction());
 ID ID_stg(.clk(clk),.rst(rst),.z(),.c(),.n(),.v(),.instruction(),.PC(),.WB_WB_EN(),.WB_Value(),.WB_Dest(),.
@@ -9,4 +11,5 @@ ID_REG ID_rsgister(.clk(clk),.rst(rst),.flush(),.carry(),.dest(),.signed_imm(),.
     EXE_CMD_out(),.MEM_W_out(),.MEM_R_out(),.WB_EN_out());
 EXE EXE_stg(.clk(clk),.rst(rst),.WB_EN(),.MEM_R(),.MEM_W(),.EXE_CMD(),.B(),.S(),.PC(),.val_rn(),.val_rm(),.imm(),.Shift_Operand(),.signed_imm(),.dest(),.carry(),.
     WB_EN_out(),.MEM_R_out(),.MEM_W_out(),.ALU_res(),.val_rm_out(),.dest_out(),.branch_adr(),.status_out(),.branch_taken());
+
 endmodule
