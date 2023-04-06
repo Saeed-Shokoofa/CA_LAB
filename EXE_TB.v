8ac27f68ac780a55be3c46efe00793ca7bc6ffbc
s_shokoofa <sd3power@gmail.com>
2023-04-06 13:59:27 +0330

    test bench changed a little

--- EXE_TB.v
+++ EXE_TB.v
@@ -8,9 +8,11 @@ wire [3:0] dest_out,status_out;
 EXE test_exe(clk,rst,WB_EN,MEM_R,MEM_W,EXE_CMD,B,S,PC,val_rn,val_rm,imm,Shift_Operand,signed_imm,dest,carry,WB_EN_out,MEM_R_out,MEM_W_out,ALU_res,val_rm_out,dest_out,branch_adr,status_out,branch_taken);
 always #10 clk=~clk;
 initial begin
-#10 rst=1;#20 rst=0;
-#30 val_rm=32'd6;val_rn=32'd10;MEM_R=1;#30 EXE_CMD=4'b0100;
-#800 $stop;
+    #10 rst=1;
+    #20 rst=0;
+    #30 val_rm=32'd6;val_rn=32'd10;MEM_R=1;
+    #30 EXE_CMD=4'b0100;
+    #800 $stop;
 
 end 
 endmodule
