5e820ae30b12c553152eb4bb6e724e808a6f0f42
s_shokoofa <sd3power@gmail.com>
2023-04-15 12:01:06 +0330

    MEM_reg completed

--- MEM_reg.v
+++ MEM_reg.v
@@ -1,1 +1,27 @@
 
+module MEM_REG(clk,rst,WB_EN,MEM_R,ALU_res,data_mem,dest,
+WB_EN_out,MEM_R_out,ALU_res_out,data_mem_out,dest_out);
+
+input clk,rst,MEM_R,WB_EN;
+input [3:0]dest;
+input [31:0]ALU_res,data_mem;
+
+output reg MEM_R_out,WB_EN_out;
+output reg [3:0]dest_out;
+output reg [31:0]ALU_res_out,data_mem_out;
+
+always@(posedge clk,posedge rst) begin
+    if(rst)
+        {WB_EN_out,MEM_R_out,ALU_res_out,data_mem_out,dest_out}<=0;
+    else begin
+        MEM_R_out<=WB_EN_out;
+        WB_EN_out<=MEM_R_out;
+        ALU_res_out<=ALU_res_out;
+        data_mem_out<=data_mem_out;
+        dest_out<=dest_out;
+    end
+end
+endmodule
+
+
+
