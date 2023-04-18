7c8577a72a73e141092729ca48bd19f3c99bb63b
s_shokoofa <sd3power@gmail.com>
2023-04-18 21:16:50 +0330

    memory stage completed

--- MEM.v
+++ MEM.v
@@ -1,11 +1,14 @@
-module MEM(input clk,input [31:0]adr,data_in,input MEM_W_EN,MEM_R_EN,WB_EN,Dest,output [31:0]DATA,adr,output Dest,MEM_R_EN,WB_EN);
-    reg [7:0]mem[0:1023];
-    always @(negedge clk)begin
-        if(MEM_R_EN)begin
-            DATA <= {mem[((adr-32'd1024))],mem[((adr-32'd1024))+32'd1],mem[((adr-32'd1024))+32'd2],mem[((adr-32'd1024))+32'd3]};
-        end
-        else if(MEM_W_EN)begin
-            {mem[((adr-32'd1024))],mem[((adr-32'd1024))+32'd1],mem[((adr-32'd1024))+32'd2],mem[((adr-32'd1024))+32'd3]} <= data_in; 
-        end
-    end
+module MEM(clk,ALU_res,val_rm,MEM_W_EN,MEM_R_EN,WB_EN,Dest,DATA,ALU_res_out,Dest_out,MEM_R_EN_out,WB_EN_out);
+input clk,MEM_W_EN,MEM_R_EN,WB_EN;
+input [3:0]Dest;
+input [31:0]ALU_res,val_rm
+output MEM_R_EN_out,WB_EN_out;
+output [3:0]Dest_out;
+output [31:0]DATA,ALU_res_out;
+memory my_mem(clk(clk),.adr(ALU_res),.data_in(val_rm),.MEM_W_EN(MEM_W_EN),.MEM_R_EN(MEM_R_EN),.DATA(DATA));
+assign ALU_res_out=ALU_res;
+assign Dest_out=Dest;
+assign MEM_R_EN_out=MEM_R_EN;
+assign WB_EN_out=WB_EN;
+
 endmodule
