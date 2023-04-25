706a42b09a981074e7718ee5a62bde488d0af734
s_shokoofa <sd3power@gmail.com>
2023-04-25 21:37:29 +0330

    forwarding unit added

--- ARM.v
+++ ARM.v
@@ -18,11 +18,12 @@ IF_reg IF_register(.clk(clk),.rst(rst),.freeze(freeze),.flush(exe_br_taken),.PC_
 ID ID_stg(.clk(clk),.rst(rst),.freeze(freeze),.z(id_z),.c(id_c),.n(id_n),.v(id_v),.instruction(if_inst_out),.PC(if_pc_out),.WB_WB_EN(WB_WB_EN),.WB_Value(WB_WB_val),.WB_Dest(WB_WB_dest),.
     WB_EN(id_WB_EN),.MEM_R(id_MEM_R),.MEM_W(id_MEM_W),.EXE_CMD(id_EXE_CMD),.B(id_B),.S(id_S),.PC_out(id_PC),.val_rn(id_val_rn),.val_rm(id_val_rm),.dest(id_dest),.signed_imm(id_signed_imm),.imm(id_imm),.Shift_Operand(id_shift_operand),.src2forhaz(src2forhaz));
 
-ID_REG ID_rsgister(.clk(clk),.rst(rst),.flush(exe_br_taken),.carry(id_c),.dest(id_dest),.signed_imm(id_signed_imm),.Shift_Operand(id_shift_operand),.imm(id_imm),.val_rm(id_val_rm),.val_rn(id_val_rn),.PC(id_PC),.S(id_S),.B(id_B),.EXE_CMD(id_EXE_CMD),.MEM_W(id_MEM_W),.MEM_R(id_MEM_R),.WB_EN(id_WB_EN),.
+ID_REG ID_rsgister(.clk(clk),.rst(rst),.flush(exe_br_taken),.carry(id_c),.dest(id_dest),.signed_imm(id_signed_imm),.Shift_Operand(id_shift_operand),.imm(id_imm),.val_rm(id_val_rm),.val_rn(id_val_rn),.PC(id_PC),.S(id_S),.B(id_B),.EXE_CMD(id_EXE_CMD),.MEM_W(id_MEM_W),.MEM_R(id_MEM_R),.WB_EN(id_WB_EN),.src1_in(if_inst_out[19:16]),.src2_in(src2forhaz),.
     carry_out(id_cry_out),.dest_out(id_dest_out),.signed_imm_out(id_signed_imm_out),.Shift_Operand_out(id_shift_operand_out),.imm_out(id_imm_out),.val_rm_out(id_val_rm_out),.val_rn_out(id_val_rn_out),.PC_out(id_PC_out),.S_out(id_S_out),.B_out(id_B_out),.
-    EXE_CMD_out(id_EXE_CMD_out),.MEM_W_out(id_MEM_W_out),.MEM_R_out(id_MEM_R_out),.WB_EN_out(id_WB_EN_out));
+    EXE_CMD_out(id_EXE_CMD_out),.MEM_W_out(id_MEM_W_out),.MEM_R_out(id_MEM_R_out),.WB_EN_out(id_WB_EN_out),.src1_out(src1_forw),.src2_out(src2_forw));
 
 EXE EXE_stg(.clk(clk),.rst(rst),.WB_EN(id_WB_EN_out),.MEM_R(id_MEM_R_out),.MEM_W(id_MEM_W_out),.EXE_CMD(id_EXE_CMD_out),.B(id_B_out),.S(id_S_out),.PC(id_PC_out),.val_rn(id_val_rn_out),.val_rm(id_val_rm_out),.imm(id_imm_out),.Shift_Operand(id_shift_operand_out),.signed_imm(id_signed_imm_out),.dest(id_dest_out),.carry(id_cry_out),.
+    MEM_val_rn(exe_ALU_res_out),.MEM_val_rm(exe_ALU_res_out),.WB_val_rn(WB_WB_val),.WB_val_rm(WB_WB_val),.sel1(sel_mux1),.sel2(sel_mux2),.
     WB_EN_out(exe_WB_EN),.MEM_R_out(exe_MEM_R),.MEM_W_out(exe_MEM_W),.ALU_res(exe_ALU_res),.val_rm_out(exe_val_rm),.dest_out(exe_dest),.branch_adr(exe_br_adr),.status_out({id_v,id_n,id_c,id_z}),.branch_taken(exe_br_taken));
 
 EXE_reg EXE_register(.clk(clk),.rst(rst),.dest(exe_dest),.val_rm(exe_val_rm),.ALU_res(exe_ALU_res),.MEM_W(exe_MEM_W),.MEM_R(exe_MEM_R),.WB_EN(exe_WB_EN),.
@@ -41,8 +42,13 @@ haz_det HDU(.src1(if_inst_out[19:16]),.src2(src2forhaz/*mux_out*/),.Two_src(id_M
 //assign freeze=1'b0;
 
 wire haz_freeze;
+wire [3:0]src1_forw,src2_forw;
+wire [1:0]sel1_forw,sel2_forw;
+wire [1:0]sel_mux1,sel_mux2;
 assign freeze=(forward_en)?0:haz_freeze;
+assign sel_mux1=(forward_en)?sel1_forw:2'b00;
+assign sel_mux2=(forward_en)?sel2_forw:2'b00;
 
-forwarding_unit FORU(.src1(),.src2(),.mem_WB(),.mem_Dest(),.WB_WB(),.WB_dest(),.sel_src1(),.sel_src2());
+forwarding_unit FORU(.src1(src1_forw),.src2(src2_forw),.mem_WB(exe_WB_EN_out),.mem_Dest(exe_dest_out),.WB_WB(MEM_WB_EN_out),.WB_dest(MEM_dest_out),.sel_src1(sel1_forw),.sel_src2(sel2_forw));
 
 endmodule
--- EXE.v
+++ EXE.v
@@ -1,8 +1,9 @@
-module EXE(clk,rst,WB_EN,MEM_R,MEM_W,EXE_CMD,B,S,PC,val_rn,val_rm,imm,Shift_Operand,signed_imm,dest,carry,
+module EXE(clk,rst,WB_EN,MEM_R,MEM_W,EXE_CMD,B,S,PC,val_rn,val_rm,imm,Shift_Operand,signed_imm,dest,carry,MEM_val_rn,MEM_val_rm,WB_val_rn,WB_val_rm,sel1,sel2,
 WB_EN_out,MEM_R_out,MEM_W_out,ALU_res,val_rm_out,dest_out,branch_adr,status_out,branch_taken);
 input clk,rst,WB_EN,MEM_R,MEM_W,B,S,imm,carry;
+input [1:0]sel1,sel2;
 input[3:0]EXE_CMD,dest;
-input [31:0]PC,val_rn,val_rm;
+input [31:0]PC,val_rn,val_rm,MEM_val_rn,MEM_val_rm,WB_val_rn,WB_val_rm;
 input [11:0]Shift_Operand;
 input [23:0]signed_imm;
 output WB_EN_out,MEM_R_out,MEM_W_out,branch_taken;
@@ -12,10 +13,14 @@ output [3:0] dest_out,status_out;
 wire [31:0] val2;
 wire [3:0] new_status;
 
+wire [31:0]mux1_out,mux2_out;
+
 adder AD(.a(PC),.b({{6{signed_imm[23]}},signed_imm[23:0],2'b0}),.out(branch_adr));
-val2gen VG(.Rm(val_rm),.Shift_Operand(Shift_Operand),.imm(imm),.en(MEM_R|MEM_W),.out(val2));
+val2gen VG(.Rm(mux2_out),.Shift_Operand(Shift_Operand),.imm(imm),.en(MEM_R|MEM_W),.out(val2));
 st_reg ST(.clk(clk),.rst(rst),.S(S),.status(new_status),.status_out(status_out));
-ALU ALU1(.src1(val_rn),.src2(val2),.cmd(EXE_CMD),.carry(carry),.out(ALU_res),.status_out(new_status));
+ALU ALU1(.src1(mux1_out),.src2(val2),.cmd(EXE_CMD),.carry(carry),.out(ALU_res),.status_out(new_status));
+mux4to1 #(32)MM1(.i0(val_rn),.i1(MEM_val_rn),.i2(WB_val_rn),.i3(),.sel(sel1),.out(mux1_out))
+mux4to1 #(32)MM2(.i0(val_rm),.i1(MEM_val_rm),.i2(WB_val_rm),.i3(),.sel(sel2),.out(mux2_out))
 
 assign branch_taken=B;
 assign dest_out=dest;
