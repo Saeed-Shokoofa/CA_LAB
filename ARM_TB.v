a7542781468ef9a709ce8cc1546496040671c453
s_shokoofa <sd3power@gmail.com>
2023-05-09 22:26:24 +0330

    SRAM was added to testbench

--- ARM_TB.v
+++ ARM_TB.v
@@ -3,8 +3,24 @@ reg clk=0,rst=0;
 reg forward_en=1;
 always #10 clk=~clk;
 
-ARM my_cpu(clk,rst,forward_en);
-ARM my_cpu2(clk,rst,~forward_en);
+wire SRAM_UB_N,SRAM_LB_N,SRAM_WE_N,SRAM_CE_N,SRAM_OE_N;
+wire [15:0]SRAM_DQ;
+wire [17:0]SRAM_ADDR;
+
+SRAM my_sram(
+    clk,
+    rst,
+    SRAM_DQ,
+    SRAM_ADDR,
+    SRAM_UB_N,
+    SRAM_LB_N,
+    SRAM_WE_N,
+    SRAM_CE_N,
+    SRAM_OE_N
+);
+ARM my_cpu(clk,rst,forward_en,SRAM_DQ,SRAM_ADDR,SRAM_UB_N,SRAM_LB_N,SRAM_WE_N,SRAM_CE_N,SRAM_OE_N);
+//ARM my_cpu(clk,rst,forward_en);
+//ARM my_cpu2(clk,rst,~forward_en);
 
 initial begin
     #5 rst=1'b1;
